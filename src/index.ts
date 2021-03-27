41423bfd3e9aa979fd3038ad5ee2d24125d4baa2
Jose Luis Leon <joseluis5000l@gmail.com>
2021-03-27 02:31:38 -0500

    Change assertions prefix
    This change aims to make migrations from Jest or Chai a lot less traumatic. Most of their assertions are written in a `toBe...` fashion and developers are already used to that prefix. If we stick to that standard, moving from those libraries will be easier and for some it'll be a big plus to have the look and feel from Jest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { expect } from "./lib/expect";
 
 export { expect };
+export { expect as assert };
 export { expect as assertThat };
 
 export default expect;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,17 @@
 import assert from "assert";
 
-import expect, { assertThat } from "../src";
+import expect, { assert as libAssert, assertThat } from "../src";
 
 describe("[Unit] index.test.ts", () => {
   context("expect", () => {
     it("is exposed to the API", () => {
       assert.ok(expect);
+    });
+  });
+
+  context("assert", () => {
+    it("is an alias of expect", () => {
+      assert.deepStrictEqual(libAssert, expect);
     });
   });
 
