ed6934b138d151a800f7121020d87398627c1de5
JoseLion <joseluis5000l@gmail.com>
2021-08-29 22:21:49 -0500

    [StringAssertion] Starter

--- src/lib/expect.ts
+++ src/lib/expect.ts
@@ -1,12 +1,14 @@
 import { Assertion } from "./Assertion";
 import { BooleanAssertion } from "./BooleanAssertion";
+import { StringAssertion } from "./StringAssertion";
 
 export function expect(actual: boolean): BooleanAssertion;
 export function expect<T>(actual: T): Assertion<T>;
 export function expect(actual: unknown): Assertion<unknown> {
-  if (typeof actual === "boolean") {
-    return new BooleanAssertion(actual);
-  }
+  switch (typeof actual) {
+    case "boolean": return new BooleanAssertion(actual);
+    case "string": return new StringAssertion(actual);
 
-  return new Assertion(actual);
+    default: return new Assertion(actual);
+  }
 }
