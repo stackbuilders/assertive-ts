2af2902ed49ff7da0290ede640fd6de44f69c50e
Jose Luis Leon <jleon@stackbuilders.com>
2021-09-03 17:15:57 -0500

    [StringAssertion] Starter (#6)
    * [StringAssertion] Starter
    
    * Address @elcuy feedback
    
    * Add aliased assertions

---
+++ src/lib/StringAssertion.ts
@@ -0,0 +1,217 @@
+import { AssertionError } from "assert";
+
+import { Assertion } from "./Assertion";
+
+export class StringAssertion extends Assertion<string> {
+
+  constructor(actual: string) {
+    super(actual);
+  }
+
+  /**
+   * Check if the string is empty. That is, when the string does not contain
+   * any characters.
+   *
+   * @returns the assertion instance
+   */
+  public toBeEmpty(): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      message: `Expected <${this.actual}> to be empty`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: "Expected the value NOT to be empty"
+    });
+
+    return this.execute({
+      assertWhen: this.actual === "",
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * Check if the string is blank. That is, when the string consists of one or
+   * more whitespaces only.
+   *
+   * @returns the assertion instance
+   */
+  public toBeBlank(): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      message: `Expected <${this.actual}> to be blank`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: "Expected the value NOT to be blank"
+    });
+
+    return this.execute({
+      assertWhen: this.actual.trimStart().trimEnd() === "",
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * Check if the string value is equal to another string. The comparison is
+   * not case sensitive, i.e. it ignores the cases of both string values.
+   *
+   * @returns the assertion instance
+   */
+  public toBeEqualIgnoringCase(text: string): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: "Expected both string to be equal ignoring case"
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: "Expected both strings NOT to be equal ignoring case"
+    });
+
+    return this.execute({
+      assertWhen: this.actual.toLowerCase() === text.toLowerCase(),
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * A convenience alias of `.toBeEqualIgnoringCase(..)` assertion.
+   *
+   * @see {@link StringAssertion.toBeEqualIgnoringCase toBeEqualIgnoringCase}
+   */
+  public toBeEqualCaseInsensitive = this.toBeEqualIgnoringCase;
+
+  /**
+   * Check if the string value contains the passed string. This check compares
+   * both strings in a case sensitive fashion.
+   *
+   * @param text the text the value should contain
+   * @returns the assertion instance
+   */
+  public toContain(text: string): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: `Expected <${this.actual}> to contain <${text}>`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: `Expected <${this.actual}> NOT to contain <${text}>`
+    });
+
+    return this.execute({
+      assertWhen: this.actual.includes(text),
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * Check if the string value contains the passed string. This check compares
+   * both strings ignoring their cases.
+   *
+   * @param text the text the value should contain (ignoring case)
+   * @returns the assertion instance
+   */
+  public toContainIgnoringCase(text: string): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: `Expected <${this.actual}> to contain <${text}> (ignoring case)`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: `Expected <${this.actual}> NOT to contain <${text}> (ignoring case)`
+    });
+
+    return this.execute({
+      assertWhen: this.actual.toLowerCase().includes(text.toLowerCase()),
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * A convenience alias of `.toContainIgnoringCase(..)` assertion.
+   *
+   * @see {@link StringAssertion.toContainIgnoringCase toContainIgnoringCase}
+   */
+  public toContainCaseInsensitive = this.toContainIgnoringCase;
+
+  /**
+   * Check if the string value starts with the passed string
+   *
+   * @param text the text he value should start with
+   * @returns the assertion instance
+   */
+  public toStartWith(text: string): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: `Expected <${this.actual}> to start with <${text}>`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: `Expected <${this.actual}> NOT to start with <${text}>`
+    });
+
+    return this.execute({
+      assertWhen: this.actual.startsWith(text),
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * Check if the string value ends with the passed string
+   *
+   * @param text the text he value should end with
+   * @returns the assertion instance
+   */
+  public toEndWith(text: string): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: `Expected <${this.actual}> to end with <${text}>`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      expected: text,
+      message: `Expected <${this.actual}> NOT to end with <${text}>`
+    });
+
+    return this.execute({
+      assertWhen: this.actual.endsWith(text),
+      error,
+      invertedError
+    });
+  }
+
+  /**
+   * Check if the string value matches a regular expression.
+   *
+   * @param regex the regular expression to match
+   * @returns the assertion instance
+   */
+  public toMatchRegex(regex: RegExp): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      message: `Expected <${this.actual}> to match the regular expression <${regex}>`
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: `Expected <${this.actual}> NOT to match the regular expression <${regex}>`
+    });
+
+    return this.execute({
+      assertWhen: regex.test(this.actual),
+      error,
+      invertedError
+    });
+  }
+}
---
+++ test/lib/StringAssertion.test.ts
@@ -0,0 +1,232 @@
+import assert from "assert";
+
+import { StringAssertion } from "../../src/lib/StringAssertion";
+
+const ASSERTION_ERROR: string = "AssertionError";
+
+describe("[Unit] StringAssertion.test.ts", () => {
+  describe(".toBeEmpty", () => {
+    context("when the value is an empty string", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("");
+
+        assert.deepStrictEqual(test.toBeEmpty(), test);
+        assert.throws(() => test.not.toBeEmpty(), {
+          message: "Expected the value NOT to be empty",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when the value NOT an empty string", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.throws(() => test.toBeEmpty(), {
+          message: "Expected <Hello World!> to be empty",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toBeEmpty(), test.not);
+      });
+    });
+  });
+
+  describe(".toBeBlank", () => {
+    context("when the value has only whitespaces", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("   ");
+
+        assert.deepStrictEqual(test.toBeBlank(), test);
+        assert.throws(() => test.not.toBeBlank(), {
+          message: "Expected the value NOT to be blank",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+    context("when the value does NOT have only whitespaces", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("  x y z  ");
+
+        assert.throws(() => test.toBeBlank(), {
+          message: "Expected <  x y z  > to be blank",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toBeBlank(), test.not);
+      });
+    });
+  });
+
+  describe(".toBeEqualIgnoringCase", () => {
+    context("when both values are equal ignoring cases", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("MiXeD TeXt");
+
+        assert.deepStrictEqual(test.toBeEqualIgnoringCase("mIxEd tExT"), test);
+        assert.throws(() => test.not.toBeEqualIgnoringCase("mIxEd tExT"), {
+          message: "Expected both strings NOT to be equal ignoring case",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when both values are NOT equal ignoring cases", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("MiXeD TeXt");
+
+        assert.throws(() => test.toBeEqualIgnoringCase("mIxEd sTrInG"), {
+          message: "Expected both string to be equal ignoring case",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toBeEqualIgnoringCase("mIxEd sTrInG"), test.not);
+      });
+    });
+  });
+
+  describe(".toBeEqualCaseInsensitive", () => {
+    it("aliases .toBeEqualIgnoringCase(..) method", () => {
+      const test = new StringAssertion("foo");
+
+      assert.deepStrictEqual(test.toBeEqualCaseInsensitive, test.toBeEqualIgnoringCase);
+      assert.deepStrictEqual(test.not.toBeEqualCaseInsensitive, test.not.toBeEqualIgnoringCase);
+    });
+  });
+
+  describe(".toContain", () => {
+    context("when the value contains the passed string", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.deepStrictEqual(test.toContain("Hello"), test);
+        assert.throws(() => test.not.toContain("Hello"), {
+          message: "Expected <Hello World!> NOT to contain <Hello>",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when the value does NOT contain the passed string", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.throws(() => test.toContain("Bye"), {
+          message: "Expected <Hello World!> to contain <Bye>",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toContain("Bye"), test.not);
+      });
+    });
+  });
+
+  describe(".toContainIgnoringCase", () => {
+    context("when the value contains the passed string ignoring case", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.deepStrictEqual(test.toContainIgnoringCase("hello"), test);
+        assert.throws(() => test.not.toContainIgnoringCase("hello"), {
+          message: "Expected <Hello World!> NOT to contain <hello> (ignoring case)",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when the value does NOT contain the passed string ignoring case", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.throws(() => test.toContainIgnoringCase("bye"), {
+          message: "Expected <Hello World!> to contain <bye> (ignoring case)",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toContainIgnoringCase("bye"), test.not);
+      });
+    });
+  });
+
+  describe(".toContainCaseInsensitive", () => {
+    it("aliases the .toContainIgnoringCase(..) method", () => {
+      const test = new StringAssertion("foo");
+
+      assert.deepStrictEqual(test.toContainCaseInsensitive, test.toContainIgnoringCase);
+      assert.deepStrictEqual(test.not.toContainCaseInsensitive, test.not.toContainIgnoringCase);
+    });
+  });
+
+  describe(".toStartWith", () => {
+    context("when the value starts with the passed text", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.deepStrictEqual(test.toStartWith("Hello"), test);
+        assert.throws(() => test.not.toStartWith("Hello"), {
+          message: "Expected <Hello World!> NOT to start with <Hello>",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when the value does NOT start with the passed text", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.throws(() => test.toStartWith("Goodbye"), {
+          message: "Expected <Hello World!> to start with <Goodbye>",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toStartWith("Goodbye"), test.not);
+      });
+    });
+  });
+
+  describe(".toEndWith", () => {
+    context("when the value ends with the passed text", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.deepStrictEqual(test.toEndWith("!"), test);
+        assert.throws(() => test.not.toEndWith("!"), {
+          message: "Expected <Hello World!> NOT to end with <!>",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when the value does NOT end with the passed text", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("Hello World!");
+
+        assert.throws(() => test.toEndWith("Moon!"), {
+          message: "Expected <Hello World!> to end with <Moon!>",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toStartWith("Moon!"), test.not);
+      });
+    });
+  });
+
+  describe(".toMatchRegex", () => {
+    context("when the value matches the regular expression", () => {
+      it("returns the assertion instance", () => {
+        const test = new StringAssertion("1234567890");
+
+        assert.deepStrictEqual(test.toMatchRegex(/^[0-9]+$/), test);
+        assert.throws(() => test.not.toMatchRegex(/^[0-9]+$/), {
+          message: "Expected <1234567890> NOT to match the regular expression </^[0-9]+$/>",
+          name: ASSERTION_ERROR
+        });
+      });
+    });
+
+    context("when the value does NOT match the regular expression", () => {
+      it("throws an assertion error", () => {
+        const test = new StringAssertion("1234567890x");
+
+        assert.throws(() => test.toMatchRegex(/^[0-9]+$/), {
+          message: "Expected <1234567890x> to match the regular expression </^[0-9]+$/>",
+          name: ASSERTION_ERROR
+        });
+        assert.deepStrictEqual(test.not.toMatchRegex(/^[0-9]+$/), test.not);
+      });
+    });
+  });
+});
