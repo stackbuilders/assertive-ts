597a22efb97c76846b34b1869f2c9aebaab5faf6
Anthony Suárez <neolight1010@gmail.com>
2022-09-16 17:22:44 -0500

    format: Format Jest example code

--- examples/jest/tests/mathUtils.test.ts
+++ examples/jest/tests/mathUtils.test.ts
@@ -1,17 +1,17 @@
-import { assert,expect } from "@stackbuilders/assertive-ts";
-import { expect as jestExpect } from "@jest/globals"
+import { assert, expect } from "@stackbuilders/assertive-ts";
+import { expect as jestExpect } from "@jest/globals";
 import { sum } from "../src/mathUtils";
 
 describe("sum", () => {
-    it("adds two numbers with assertive-ts' expect", () => {
-        expect(sum(1, 2)).toBeEqual(3);
-    });
+  it("adds two numbers with assertive-ts' expect", () => {
+    expect(sum(1, 2)).toBeEqual(3);
+  });
 
-    it("adds two numbers with assertive-ts' assert", () => {
-        assert(sum(1, 2)).toBeEqual(3);
-    });
+  it("adds two numbers with assertive-ts' assert", () => {
+    assert(sum(1, 2)).toBeEqual(3);
+  });
 
-    it("adds two numbers with jest's expect", () => {
-        jestExpect(sum(1, 2)).toEqual(3);
-    });
+  it("adds two numbers with jest's expect", () => {
+    jestExpect(sum(1, 2)).toEqual(3);
+  });
 });
