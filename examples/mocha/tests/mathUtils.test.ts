537967e09f4092b3562a1acf107d62ee84bf34f1
Anthony Suárez <neolight1010@gmail.com>
2022-09-16 17:23:42 -0500

    format: Format Mocha example code

--- examples/mocha/tests/mathUtils.test.ts
+++ examples/mocha/tests/mathUtils.test.ts
@@ -2,11 +2,11 @@ import { assert, expect } from "@stackbuilders/assertive-ts";
 import { sum } from "../src/mathUtils";
 
 describe("sum", () => {
-    it("adds two numbers with expect", () => {
-        expect(sum(1, 2)).toBeEqual(3);
-    });
+  it("adds two numbers with expect", () => {
+    expect(sum(1, 2)).toBeEqual(3);
+  });
 
-    it("adds two numbers with assert", () => {
-        assert(sum(1, 2)).toBeEqual(3);
-    });
+  it("adds two numbers with assert", () => {
+    assert(sum(1, 2)).toBeEqual(3);
+  });
 });
