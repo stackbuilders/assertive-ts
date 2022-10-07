69b6564b745a6b97e956f44e42d9aa195107849a
Anthony Suárez <neolight1010@gmail.com>
2022-10-07 08:39:42 -0500

    fix: Convert "sunday" to 0 instead of 7
    This makes the implementation consistent with JS' Date.getDay() and with
    DateAssertion.toBeDayOfWeek()'s documentation.

--- src/lib/helpers/dates.ts
+++ src/lib/helpers/dates.ts
@@ -2,13 +2,13 @@ import { DateOptions, DayOfWeek, Month } from "../DateAssertion.types";
 
 export function dayOfWeekAsNumber (day: DayOfWeek): number {
     switch (day) {
+      case "sunday": return 0;
       case "monday": return 1;
       case "tuesday": return 2;
       case "wednesday": return 3;
       case "thursday": return 4;
       case "friday": return 5;
       case "saturday": return 6;
-      case "sunday": return 7;
     }
   }
 
