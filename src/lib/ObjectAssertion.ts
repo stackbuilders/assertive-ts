a0e1222689516813029f80f197b9c5396a7cc8a9
Anthony Suárez <neolight1010@gmail.com>
2022-10-09 11:44:22 -0500

    docs: Add examples to ObjectAssertion matchers
    Added examples to the following ObjectAssertion matcher methods:
    - .toBeEmpty()
    - .toContainKey()
    - .toContainAllKeys()
    - .toContainAnyKeys()
    - .toContainValue()
    - .toContainAllValues()
    - .toContainAnyValues()
    - .toContainEntry()
    - .toContainAllEntries()
    - .toContainAnyEntries()
    - .toPartiallyMatch()

--- src/lib/ObjectAssertion.ts
+++ src/lib/ObjectAssertion.ts
@@ -23,6 +23,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
   /**
    * Check if the object is empty. That is, when the object doesn't have any properties.
    *
+   * @example
+   * ```
+   * expect({}).toBeEmpty();
+   * ```
+   *
    * @returns the assertion instance
    */
   public toBeEmpty(): this {
@@ -44,6 +49,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
 
   /**
    * Check if the object contains the provided key.
+   *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2 }).toContainKey("a");
+   * ```
    *
    * @param key the key that the object should contain
    * @returns the assertion instance
@@ -68,6 +78,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
   /**
    * Check if the object contains the all provided keys.
    *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainAllKeys(["a", "b"]);
+   * ```
+   *
    * @param keys the keys that the object should contain
    * @returns the assertion instance
    */
@@ -90,6 +105,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
 
   /**
    * Check if the object contains at least one of the provided keys.
+   *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainAnyKeys(["a", "b"]);
+   * ```
    *
    * @param keys the keys that the object may contain
    * @returns the assertion instance
@@ -114,6 +134,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
   /**
    * Check if the object contains the provided value.
    *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainValue(2);
+   * ```
+   *
    * @param value the property value that the object should contain in any of its keys
    * @returns the assertion instance
    */
@@ -136,6 +161,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
 
   /**
    * Check if the object contains all the provided values.
+   *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainAllValues([1, 2]);
+   * ```
    *
    * @param values the property values that the object should contain
    * @returns the assertion instance
@@ -163,6 +193,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
   /**
    * Check if the object contains at least one of the provided values.
    *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainAnyValues([1, 5, 7]);
+   * ```
+   *
    * @param values the property values that the object should contain
    * @returns the assertion instance
    */
@@ -189,6 +224,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
   /**
    * Check if the object contains the provided entry.
    *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainEntry(["a", 1]);
+   * ```
+   *
    * @param entry the entry that the object should contain
    * @returns the assertion instance
    */
@@ -213,6 +253,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
 
   /**
    * Check if the object contains all the provided entries.
+   *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toContainAllEntries(["a", 1], ["b", 2]);
+   * ```
    *
    * @param entries the entries that the object should contain
    * @returns the assertion instance
@@ -241,6 +286,12 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
   /**
    * Check if the object contains at least one of the provided entries.
    *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 })
+   *   .toContainAnyEntries(["a", 1], ["b", 9], ["c", 20]);
+   * ```
+   *
    * @param entries the entries that the object should contain
    * @returns the assertion instance
    */
@@ -267,6 +318,11 @@ export class ObjectAssertion<T extends JSObject> extends Assertion<T> {
 
   /**
    * Check if the object match the provided object.
+   *
+   * @example
+   * ```
+   * expect({ a: 1, b: 2, c: 3 }).toPartiallyMatch({ b: 2, c: 3 });
+   * ```
    *
    * @param other the object that the object should match
    * @returns the assertion instance
