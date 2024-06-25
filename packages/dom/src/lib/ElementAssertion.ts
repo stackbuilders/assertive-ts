c71daf5338f393c734e324442a800ff13be781a5
Christian Samaniego <christiansama1601@gmail.com>
2024-06-25 13:32:59 -0500

    feat(dom): DOM Element Assertion (#129)
    Co-authored-by: matycarolina <contrerasmarialejandra1@gmail.com>
    Co-authored-by: Edwin Hernandez <edhernandez@stackbuilders.com>

---
+++ packages/dom/src/lib/ElementAssertion.ts
@@ -0,0 +1,33 @@
+import { Assertion, AssertionError } from "@assertive-ts/core";
+
+export class ElementAssertion<T extends Element> extends Assertion<T> {
+
+  public constructor(actual: T) {
+    super(actual);
+  }
+
+  /**
+   * Check if the element is in the document.
+   *
+   * @returns the assertion instance.
+   */
+  public toBeInTheDocument(): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      message: "Expected the element to be in the document",
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: "Expected the element to NOT be in the document",
+    });
+
+    return this.execute({
+      assertWhen: (
+        this.actual.ownerDocument.defaultView !== null
+        && this.actual.ownerDocument === this.actual.getRootNode({ composed: true })
+      ),
+      error,
+      invertedError,
+    });
+  }
+}
---
+++ packages/dom/test/unit/lib/ElementAssertion.test.tsx
@@ -0,0 +1,45 @@
+import { AssertionError, expect } from "@assertive-ts/core";
+import { render } from "@testing-library/react";
+import { ReactElement } from "react";
+
+import { ElementAssertion } from "../../../src/lib/ElementAssertion";
+
+function TestComponent(): ReactElement {
+  return (
+    <div>
+      <button>click me</button>
+    </div>
+  );
+}
+
+describe("[Unit] ElementAssertion.test.ts", () => {
+  describe(".toBeInTheDocument", () => {
+    context("when the element is in the document", () => {
+      it("returns the assertion instance", async () => {
+        const { findByRole } = render(<TestComponent />);
+        const button = await findByRole("button", { name: "click me" });
+        const test = new ElementAssertion(button);
+
+        expect(test.toBeInTheDocument()).toBeEqual(test);
+
+        expect(() => test.not.toBeInTheDocument())
+          .toThrowError(AssertionError)
+          .toHaveMessage("Expected the element to NOT be in the document");
+      });
+    });
+
+    context("when the element is not in the document", () => {
+      it("throws an assertion error", () => {
+        const detachedElement = document.createElement("div");
+
+        const test = new ElementAssertion(detachedElement);
+
+        expect(() => test.toBeInTheDocument())
+          .toThrowError(AssertionError)
+          .toHaveMessage("Expected the element to be in the document");
+
+        expect(test.not.toBeInTheDocument()).toBeEqual(test);
+      });
+    });
+  });
+});
