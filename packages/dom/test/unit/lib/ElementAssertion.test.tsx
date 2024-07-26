975c8b295afa4507763c9da1239873337cbb0a4c
Edwin Hernández <67283172+fonsiher@users.noreply.github.com>
2024-07-26 15:53:46 -0500

    feat(dom): DOM - toHaveAttibute (#131)

--- packages/dom/test/unit/lib/ElementAssertion.test.tsx
+++ packages/dom/test/unit/lib/ElementAssertion.test.tsx
@@ -12,6 +12,14 @@ function TestComponent(): ReactElement {
   );
 }
 
+function TestComponentElement(): ReactElement {
+  return (
+    <button role="button" type="submit" className="btn primary" disabled>
+      click me
+    </button>
+  );
+}
+
 describe("[Unit] ElementAssertion.test.ts", () => {
   describe(".toBeInTheDocument", () => {
     context("when the element is in the document", () => {
@@ -39,6 +47,65 @@ describe("[Unit] ElementAssertion.test.ts", () => {
           .toHaveMessage("Expected the element to be in the document");
 
         expect(test.not.toBeInTheDocument()).toBeEqual(test);
+      });
+    });
+  });
+
+  describe(".toHaveAttribute", () => {
+    context("when the element has the attribute with the expected value", () => {
+      it("returns the assertion instance", async () => {
+        const { findByRole } = render(<TestComponentElement />);
+        const button = await findByRole("button", { name: "click me" });
+        const test = new ElementAssertion(button);
+
+        expect(test.toHaveAttribute("type", "submit")).toBeEqual(test);
+
+        expect(() => test.not.toHaveAttribute("type", "submit"))
+          .toThrowError(AssertionError)
+          .toHaveMessage("Expected to NOT have attribute \"type\" with value \"submit\", but received \"submit\"");
+      });
+    });
+
+    context("when the element has the attribute with a not expected value", () => {
+      it("throws an assertion error", async () => {
+        const { findByRole } = render(<TestComponentElement />);
+        const button = await findByRole("button", { name: "click me" });
+        const test = new ElementAssertion(button);
+
+        expect(() => test.toHaveAttribute("type", "different value"))
+          .toThrowError(AssertionError)
+          .toHaveMessage("Expected to have attribute \"type\" with value \"different value\", but received \"submit\"",
+          );
+
+        expect(test.not.toHaveAttribute("type", "different value")).toBeEqual(test);
+      });
+    });
+
+    context("when the element has the attribute without checking value", () => {
+      it("returns the assertion instance", async () => {
+        const { findByRole } = render(<TestComponentElement />);
+        const button = await findByRole("button", { name: "click me" });
+        const test = new ElementAssertion(button);
+
+        expect(test.toHaveAttribute("disabled")).toBeEqual(test);
+
+        expect(() => test.not.toHaveAttribute("disabled"))
+          .toThrowError(AssertionError)
+          .toHaveMessage("Expected to NOT have attribute \"disabled\"");
+      });
+    });
+
+    context("when the element does not have the attribute", () => {
+      it("throws an assertion error", async () => {
+        const { findByRole } = render(<TestComponentElement />);
+        const button = await findByRole("button", { name: "click me" });
+        const test = new ElementAssertion(button);
+
+        expect(() => test.toHaveAttribute("non-existent"))
+          .toThrowError(AssertionError)
+          .toHaveMessage("Expected to have attribute \"non-existent\"");
+
+        expect(test.not.toHaveAttribute("non-existent")).toBeEqual(test);
       });
     });
   });
