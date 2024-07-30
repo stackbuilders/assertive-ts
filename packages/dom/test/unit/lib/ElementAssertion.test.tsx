db0497c8c0958333a3305fa630957e7d97921273
matycarolina <contrerasmarialejandra1@gmail.com>
2024-07-30 10:59:25 -0500

    Update toContainElement tests

--- packages/dom/test/unit/lib/ElementAssertion.test.tsx
+++ packages/dom/test/unit/lib/ElementAssertion.test.tsx
@@ -1,27 +1,8 @@
 import { AssertionError, expect } from "@assertive-ts/core";
 import { render } from "@testing-library/react";
-import { ReactElement } from "react";
 
 import { ElementAssertion } from "../../../src/lib/ElementAssertion";
-
-function TestComponent(): ReactElement {
-  return (
-    <div>
-      <button>click me</button>
-    </div>
-  );
-}
-
-function ContainsTestComponent(): ReactElement {
-  return (
-    <span data-testid="grandparent">
-      <span data-testid="parent">
-        <span data-testid="child"></span>
-      </span>
-      <svg data-testid="svg-element"></svg>
-    </span>
-  );
-}
+import { TestComponent, ContainsTestComponent } from "../../../src/lib/resources/fixtures";
 
 describe("[Unit] ElementAssertion.test.ts", () => {
   describe(".toBeInTheDocument", () => {
@@ -55,78 +36,86 @@ describe("[Unit] ElementAssertion.test.ts", () => {
   });
 
   describe(".toContainElement", () => {
-    context("when the ancestor element contains the descendant element", () => {
-      it("returns the assertion instance", () => {
-        const { getByTestId } = render(<ContainsTestComponent/>);
+    context("when the descendant element is contained in the ancestor element", () => {
+      context("and it is a direct child", () => {
+        it("returns the assertion instance", () => {
+          const { getByTestId } = render(<ContainsTestComponent/>);
 
-        const grandparent = getByTestId("grandparent");
-        const parent = getByTestId("parent");
-        const child = getByTestId("child");
-        const svgElement = getByTestId("svg-element");
+          const grandparent = getByTestId("grandparent");
+          const parent = getByTestId("parent");
+          const child = getByTestId("child");
+          const svgElement = getByTestId("svg-element");
 
-        const grandparentTest = new ElementAssertion(grandparent);
-        const parentTest = new ElementAssertion(parent);
+          const grandparentTest = new ElementAssertion(grandparent);
+          const parentTest = new ElementAssertion(parent);
 
-        expect(grandparentTest.toContainElement(parent));
-        expect(grandparentTest.toContainElement(child));
-        expect(grandparentTest.toContainElement(svgElement));
-        expect(parentTest.toContainElement(child));
+          expect(grandparentTest.toContainElement(parent));
+          expect(grandparentTest.toContainElement(svgElement));
+          expect(parentTest.toContainElement(child));
 
-        expect(() => grandparentTest.not.toContainElement(parent))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to NOT contain the element");
+          expect(() => grandparentTest.not.toContainElement(parent))
+            .toThrowError(AssertionError)
+            .toHaveMessage("Expected the container to NOT contain the element");
 
-        expect(() => grandparentTest.not.toContainElement(child))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to NOT contain the element");
+          expect(() => grandparentTest.not.toContainElement(svgElement))
+            .toThrowError(AssertionError)
+            .toHaveMessage("Expected the container to NOT contain the element");
 
-        expect(() => grandparentTest.not.toContainElement(svgElement))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to NOT contain the element");
+          expect(() => parentTest.not.toContainElement(child))
+            .toThrowError(AssertionError)
+            .toHaveMessage("Expected the container to NOT contain the element");
+        });
+      });
 
-        expect(() => parentTest.not.toContainElement(child))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to NOT contain the element");
+      context("and it is an indirect child", () => {
+        it("returns the assertion instance", () => {
+          const { getByTestId } = render(<ContainsTestComponent/>);
+
+          const grandparent = getByTestId("grandparent");
+          const child = getByTestId("child");
+
+          const grandparentTest = new ElementAssertion(grandparent);
+
+          expect(grandparentTest.toContainElement(child));
+
+          expect(() => grandparentTest.not.toContainElement(child))
+            .toThrowError(AssertionError)
+            .toHaveMessage("Expected the container to NOT contain the element");
+        });
+      });
+
+      context("and it is a deeply nested child", () => {
+        it("returns the assertion instance", () => {
+          const { getByTestId } = render(<ContainsTestComponent/>);
+
+          const grandparent = getByTestId("grandparent");
+          const deepChild = getByTestId("deep-child");
+
+          const grandparentTest = new ElementAssertion(grandparent);
+
+          expect(grandparentTest.toContainElement(deepChild));
+
+          expect(() => grandparentTest.not.toContainElement(deepChild))
+            .toThrowError(AssertionError)
+            .toHaveMessage("Expected the container to NOT contain the element");
+        });
       });
     });
 
-    context("when the descendant element does not contain the ancestor element", () => {
-      it("returns the assertion instance", () => {
+    context("when element is NOT contained in ancestor element", () => {
+      it("throws an assertion error", () => {
+        const detachedElement = document.createElement("span") as Element;
         const { getByTestId } = render(<ContainsTestComponent/>);
 
-        const grandparent = getByTestId("grandparent");
-        const parent = getByTestId("parent");
-        const child = getByTestId("child");
-        const svgElement = getByTestId("svg-element");
+          const grandparent = getByTestId("grandparent");
 
-        const parentTest = new ElementAssertion(parent);
-        const childTest = new ElementAssertion(child);
+          const grandparentTest = new ElementAssertion(grandparent);
 
-        expect(parentTest.not.toContainElement(grandparent));
-        expect(parentTest.not.toContainElement(svgElement));
-        expect(childTest.not.toContainElement(grandparent));
-        expect(childTest.not.toContainElement(parent));
-        expect(childTest.not.toContainElement(svgElement));
-
-        expect(() => parentTest.toContainElement(grandparent))
+        expect(() => grandparentTest.toContainElement(detachedElement))
           .toThrowError(AssertionError)
           .toHaveMessage("Expected the container to contain the element");
 
-        expect(() => parentTest.toContainElement(svgElement))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to contain the element");
-
-        expect(() => childTest.toContainElement(grandparent))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to contain the element");
-
-        expect(() => childTest.toContainElement(parent))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to contain the element");
-
-        expect(() => childTest.toContainElement(svgElement))
-          .toThrowError(AssertionError)
-          .toHaveMessage("Expected the container to contain the element");
+        expect(grandparentTest.not.toContainElement(detachedElement)).toBeEqual(grandparentTest);
       });
     });
   });
