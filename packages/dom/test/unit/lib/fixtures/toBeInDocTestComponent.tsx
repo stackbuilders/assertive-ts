68fc8014cce9b46d648e29b4b40e39a9e64d84ca
matycarolina <contrerasmarialejandra1@gmail.com>
2024-08-13 11:28:04 -0500

    Update fixtures folder

---
+++ packages/dom/test/unit/lib/fixtures/toBeInDocTestComponent.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export function TestComponent(): ReactElement {
+    return (
+      <div>
+        <button>click me</button>
+      </div>
+    );
+  }
---
+++ packages/dom/test/unit/lib/fixtures/toContainElementTestComponent.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react";
+
+export function ContainsTestComponent(): ReactElement {
+  return (
+    <span data-testid="grandparent">
+      <span data-testid="parent">
+        <span data-testid="child">
+          <span>
+              <span>
+                  <span>
+                      <span data-testid="deep-child"></span>
+                  </span>
+              </span>
+          </span>
+        </span>
+      </span>
+      <svg data-testid="svg-element"></svg>
+    </span>
+  );
+}
