5a8f357cc8e969115e6c515e1a775b918e13208b
suany0805 <suany_0805@hotmail.com>
2024-11-06 15:56:46 -0500

    WIP toBeDisabled

---
+++ packages/native/src/utils.ts
@@ -0,0 +1,19 @@
+import { ReactTestInstance } from "react-test-renderer";
+
+export function checkReactElement(
+    element: ReactTestInstance | null | undefined
+  ): asserts element is ReactTestInstance {
+    if (!element) {
+      throw new Error('Value must be a React element');
+    }
+  
+    // @ts-expect-error internal _fiber property of ReactTestInstance
+    if (!element._fiber && !VALID_ELEMENTS.includes(element.type.toString())) {
+        throw new Error('Value must be a React element');
+    }
+  }
+
+export function getType({ type }: ReactTestInstance) {
+    // @ts-expect-error: ReactTestInstance contains too loose typing
+    return type.displayName || type.name || type;
+  }
