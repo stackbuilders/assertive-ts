39ff2b3f51f72645eccd20efaffd9d03fafa14ee
suany0805 <suany_0805@hotmail.com>
2024-11-14 15:14:55 -0500

    WIP toBeDisabled

--- packages/native/src/lib/ElementAssertion.ts
+++ packages/native/src/lib/ElementAssertion.ts
@@ -1,9 +1,65 @@
-import { Assertion } from "@assertive-ts/core";
+import { Assertion, AssertionError } from "@assertive-ts/core";
 import { ReactTestInstance } from "react-test-renderer";
 
+// Elements that support 'disabled'
+const DISABLE_TYPES = [
+    "Button",
+    "Slider",
+    "Switch",
+    "Text",
+    "TouchableHighlight",
+    "TouchableOpacity",
+    "TouchableWithoutFeedback",
+    "TouchableNativeFeedback",
+    "View",
+    "TextInput",
+    "Pressable",
+  ];
 export class ElementAssertion extends Assertion<ReactTestInstance> {
     public constructor(actual: ReactTestInstance) {
         super(actual);
     }
+
+    public toBeDisabled(): this {
+        const error = new AssertionError({
+            actual: this.actual,
+            message: `Expected <${this.actual}> to be disabled`,
+          });
+          const invertedError = new AssertionError({
+            actual: this.actual,
+            message: "Expected the value NOT to be empty",
+          });
+
+          return this.execute({
+            assertWhen: this.isElementDisabled(this.actual) || this.isAncestorDisabled(this.actual),
+            error,
+            invertedError,
+          });
+    }
+
+    private isElementDisabled(element: ReactTestInstance): boolean {
+        if (this.getType(element) === "TextInput" && element?.props?.editable === false) {
+          return true;
+        }
+
+        if (!DISABLE_TYPES.includes(this.getType(element))) {
+          return false;
+        }
+
+        return (
+          !!element?.props?.disabled ||
+          !!element?.props?.accessibilityState?.disabled ||
+          !!element?.props?.accessibilityStates?.includes("disabled")
+        );
+      }
+
+    private isAncestorDisabled(element: ReactTestInstance): boolean {
+        const parent = element.parent;
+        return parent !== null && (this.isElementDisabled(element) || this.isAncestorDisabled(parent));
+      }
+    private getType({ type }: ReactTestInstance): string {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return type.displayName || type.name || type;
+      }
 
 }
--- packages/native/src/utils.ts
+++ packages/native/src/utils.ts
@@ -1,19 +1,6 @@
 import { ReactTestInstance } from "react-test-renderer";
 
-export function checkReactElement(
-    element: ReactTestInstance | null | undefined
-  ): asserts element is ReactTestInstance {
-    if (!element) {
-      throw new Error('Value must be a React element');
-    }
-  
-    // @ts-expect-error internal _fiber property of ReactTestInstance
-    if (!element._fiber && !VALID_ELEMENTS.includes(element.type.toString())) {
-        throw new Error('Value must be a React element');
-    }
-  }
-
-export function getType({ type }: ReactTestInstance) : string {
+export function getType({ type }: ReactTestInstance): string {
     // @ts-expect-error: ReactTestInstance contains too loose typing
     return type.displayName || type.name || type;
   }
