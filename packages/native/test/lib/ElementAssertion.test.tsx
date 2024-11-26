e0cb309929704d37dfa33077a26b06e2df1a1493
Carolina Lopez <calopez@twilio.com>
2024-11-26 16:32:18 -0500

    Add more tests

--- packages/native/test/lib/ElementAssertion.test.tsx
+++ packages/native/test/lib/ElementAssertion.test.tsx
@@ -1,9 +1,25 @@
 import { render } from "@testing-library/react-native";
-import { TextInput } from "react-native";
+import {
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableNativeFeedback,
+  View,
+  TextInput,
+  Pressable,
+} from "react-native";
 
 import { ElementAssertion } from "../../src/lib/ElementAssertion";
 
 import assert, { AssertionError } from "assert";
+
+const ALLOWED_COMPONENTS = {
+  Pressable,
+  TextInput,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,
+  View,
+};
 
 describe("[Unit] ElementAssertion.test.ts", () => {
     describe(".toBeDisabled", () => {
@@ -14,6 +30,23 @@ describe("[Unit] ElementAssertion.test.ts", () => {
           );
           const test = new ElementAssertion(element.getByTestId("id"));
 
+          assert.deepStrictEqual(test.toBeDisabled(), test);
+          assert.throws(() => test.not.toBeDisabled(), {
+            message: "Expected the value NOT to be disabled",
+            name: AssertionError.name,
+          });
+        });
+      });
+
+      Object.entries(ALLOWED_COMPONENTS).forEach(([name, Component]) => {
+        it(`handle disabled prop for element ${name}`, () => {
+          const element = render(
+              // @ts-expect-error JSX element 'Component'
+              <Component disabled={true} testID={name}>
+                <TextInput />
+              </Component>,
+          );
+          const test = new ElementAssertion(element.getByTestId(name));
           assert.deepStrictEqual(test.toBeDisabled(), test);
           assert.throws(() => test.not.toBeDisabled(), {
             message: "Expected the value NOT to be disabled",
