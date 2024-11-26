7dd25a0eab43637e4b61774ce9a8785f3c0b851f
Carolina Lopez <calopez@twilio.com>
2024-11-26 16:10:15 -0500

    Remove unneded files

--- packages/native/src/utils.ts
+++
@@ -1,6 +0,0 @@
-import { ReactTestInstance } from "react-test-renderer";
-
-export function getType({ type }: ReactTestInstance): string {
-    // @ts-expect-error: ReactTestInstance contains too loose typing
-    return type.displayName || type.name || type;
-  }
