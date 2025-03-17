cb4fbbf7defce484cda54e0e44ca484785d83e63
Karla Quistanchala <karly.btr.97@hotmail.com>
2025-03-17 10:47:13 -0500

    WIP: toBeEmpty function

---
+++ packages/native/src/lib/ToBeEmptyElementAssertion.ts
@@ -0,0 +1,51 @@
+import { Assertion, AssertionError } from "@assertive-ts/core";
+import { ReactTestInstance } from "react-test-renderer";
+
+export class ToBeEmptyElementAssertion extends Assertion<ReactTestInstance> {
+  public constructor(actual: ReactTestInstance) {
+    super(actual);
+  }
+
+  public override toString = (): string => {
+    if (this.actual === null) {
+      return "null";
+    }
+
+    return `<${this.actual.type.toString()} ... />`;
+  };
+
+  /**
+   * Check if the element is empty.
+   *
+   * @example
+   * ```
+   * expect(element).toBeEmptyElement();
+   * ```
+   *
+   * @returns the assertion instance
+   */
+  public toBeEmptyElement(): this {
+    const error = new AssertionError({
+      actual: this.actual,
+      message: `Expected element ${this.toString()} to be empty.`,
+    });
+    const invertedError = new AssertionError({
+      actual: this.actual,
+      message: `Expected element ${this.toString()} to NOT be empty.`,
+    });
+
+    return this.execute({
+      assertWhen: this.isEmpty(this.actual),
+      error,
+      invertedError,
+    });
+  }
+
+  private isEmpty(element: ReactTestInstance): boolean {
+    if(!element?.props?.children) {
+      return true;
+    }
+
+    return element?.props?.children.length === 0;
+  }
+}
