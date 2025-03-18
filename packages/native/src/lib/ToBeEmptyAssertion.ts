5c7300bc97bd9eba62acad2c69a182378d9e1705
Karla Quistanchala <karly.btr.97@hotmail.com>
2025-03-18 13:01:54 -0500

    Update: CR comments

--- packages/native/src/lib/ToBeEmptyAssertion.ts
+++ packages/native/src/lib/ToBeEmptyAssertion.ts
@@ -1,22 +1,18 @@
 import { Assertion, AssertionError } from "@assertive-ts/core";
 import { ReactTestInstance } from "react-test-renderer";
 
-import { isEmpty } from "./helpers/helpers";
+import { instanceToString, isEmpty } from "./helpers/helpers";
 
 /**
  * Assertion for checking if a React element is empty.
  */
-export class ToBeEmptyElementAssertion extends Assertion<ReactTestInstance> {
+export class ToBeEmptyAssertion extends Assertion<ReactTestInstance> {
   public constructor(actual: ReactTestInstance) {
     super(actual);
   }
 
   public override toString = (): string => {
-    if (this.actual === null) {
-      return "null";
-    }
-
-    return `<${this.actual.type.toString()} ... />`;
+    return instanceToString(this.actual);
   };
 
   /**
@@ -24,19 +20,19 @@ export class ToBeEmptyElementAssertion extends Assertion<ReactTestInstance> {
    *
    * @example
    * ```
-   * expect(element).toBeEmptyElement();
+   * expect(element).toBeEmpty();
    * ```
    *
    * @returns the assertion instance
    */
-  public toBeEmptyElement(): this {
+  public toBeEmpty(): this {
     const error = new AssertionError({
       actual: this.actual,
       message: `Expected element ${this.toString()} to be empty.`,
     });
     const invertedError = new AssertionError({
       actual: this.actual,
-      message: `Expected element ${this.toString()} to NOT be empty.`,
+      message: `Expected element ${this.toString()} NOT to be empty.`,
     });
 
     return this.execute({
