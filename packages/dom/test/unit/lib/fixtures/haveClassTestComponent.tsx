a831c903dda0dee24fc0a7d244ca9bdc281a4c0f
Edwin Hernandez <edhernandez@stackbuilders.com>
2025-04-03 12:24:05 -0500

    Add new matches based on the class input

--- packages/dom/test/unit/lib/fixtures/haveClassTestComponent.tsx
+++ packages/dom/test/unit/lib/fixtures/haveClassTestComponent.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 export function HaveClassTestComponent(): ReactElement {
     return (
       <div data-testid="classTest">
-        Test text inside a div
+        {"Test text inside a div"}
       </div>
     );
 }
