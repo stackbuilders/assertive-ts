a88cfd4c83900a7ff08b421606a58975240160df
Sebas Cruz <sebas.cruz750@gmail.com>
2026-04-01 15:36:57 -0500

    refactor: removed comments from helpers and types

--- packages/native/src/lib/helpers/helpers.ts
+++ packages/native/src/lib/helpers/helpers.ts
@@ -3,12 +3,6 @@ import { ReactTestInstance } from "react-test-renderer";
 
 import { AssertiveStyle, StyleObject, TestableTextMatcher } from "./types";
 
-/**
- * Checks if a value is empty.
- *
- * @param value - The value to check.
- * @returns `true` if the value is empty, `false` otherwise.
- */
 export function isEmpty(value: unknown): boolean {
   if (!value) {
     return true;
@@ -21,12 +15,6 @@ export function isEmpty(value: unknown): boolean {
   return false;
 }
 
-/**
- * Converts a ReactTestInstance to a string representation.
- *
- * @param instance - The ReactTestInstance to convert.
- * @returns A string representation of the instance.
- */
 export function instanceToString(instance: ReactTestInstance | null): string {
   if (instance === null) {
     return "null";
@@ -35,23 +23,6 @@ export function instanceToString(instance: ReactTestInstance | null): string {
   return `<${instance.type.toString()} ... />`;
 }
 
-/**
- * Checks if a text matches a given matcher.
- *
- * @param text - The text to check.
- * @param matcher - The matcher to use for comparison.
- * @returns `true` if the text matches the matcher, `false` otherwise.
- * @throws Error if the matcher is not a string, RegExp, or function.
- * @example
- * ```ts
- * textMatches("Hello World", "Hello World"); // true
- * textMatches("Hello World", /Hello/); // true
- * textMatches("Hello World", (text) => text.startsWith("Hello")); // true
- * textMatches("Hello World", "Goodbye"); // false
- * textMatches("Hello World", /Goodbye/); // false
- * textMatches("Hello World", (text) => text.startsWith("Goodbye")); // false
- * ```
- */
 export function textMatches(
   text: string,
   matcher: TestableTextMatcher,
