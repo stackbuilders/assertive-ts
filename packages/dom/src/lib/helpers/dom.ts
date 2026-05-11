d4d40c53ddbee9872783d5857b7d49a75497f85d
kmunoz <keylamgmg@gmail.com>
2026-05-11 11:49:40 -0500

    feat(dom): toBePressed & toBePartiallyPressed

--- packages/dom/src/lib/helpers/dom.ts
+++ packages/dom/src/lib/helpers/dom.ts
@@ -4,3 +4,23 @@ export function isElementEmpty(element: Element): boolean {
   const nonCommentChildNodes = [...element.childNodes].filter(child => child.nodeType !== COMMENT_NODE_TYPE);
   return nonCommentChildNodes.length === 0;
 }
+
+export function isButtonElement(element: Element): boolean {
+  const roles = (element.getAttribute("role") || "")
+    .split(" ")
+    .map(role => role.trim());
+
+  const tagName = element.tagName.toLowerCase();
+  const type = element.getAttribute("type");
+
+  return (
+    tagName === "button"
+    || (tagName === "input" && type === "button")
+    || roles.includes("button")
+  );
+}
+
+export function isValidAriaPressed(element: Element): boolean {
+  const pressedAttribute = element.getAttribute("aria-pressed");
+  return pressedAttribute === "true" || pressedAttribute === "false" || pressedAttribute === "mixed";
+}
