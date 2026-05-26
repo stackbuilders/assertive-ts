1c8407e43d5826c8012ffe4b8dd5f1dde1a1807c
KeylaMunnoz <109166857+KeylaMunnoz@users.noreply.github.com>
2026-05-26 11:03:40 -0500

    Refactor isValidAriaPressed to use includes method

--- packages/dom/src/lib/helpers/accessibility.ts
+++ packages/dom/src/lib/helpers/accessibility.ts
@@ -31,5 +31,5 @@ export function getAccessibleDescription(actual: Element): string {
 
 export function isValidAriaPressed(element: Element): boolean {
   const pressedAttribute = element.getAttribute("aria-pressed");
-  return pressedAttribute === "true" || pressedAttribute === "false" || pressedAttribute === "mixed";
+  return ["true", "false", "mixed"].includes(pressedAttribute);
 }
